77b6d1ec01155b7a562311209a515ee303932ed7
Muskan <abc.gmail.com>
2023-02-04 19:40:21 +0530

    first commit

---
+++ src/main/java/mypkg/MyClass.java
@@ -0,0 +1,17 @@
+package mypkg;
+
+public class MyClass {
+	public void show()
+	{
+		System.out.println("Hello Muskan!! welcome to jenkins world");
+	}
+
+	public static void main(String[] args) {
+		MyClass ob=new MyClass();
+		ob.show();
+		
+		
+
+	}
+
+}
